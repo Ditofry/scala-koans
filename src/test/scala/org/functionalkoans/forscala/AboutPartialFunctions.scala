cedc45449eb69cf4d4041d6d2e200d2e14dae9e1
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2015-09-29 16:31:43 -0600

    partial, partial apply

--- src/test/scala/org/functionalkoans/forscala/AboutPartialFunctions.scala
+++ src/test/scala/org/functionalkoans/forscala/AboutPartialFunctions.scala
@@ -10,6 +10,7 @@ class AboutPartialFunctions extends KoanSuite with ShouldMatchers {
           | a solution.  The trait PartialFunction requires that the
           | the method isDefinedAt and apply be implemented.""") {
 
+    // Sweet sassy molassy
     val doubleEvens: PartialFunction[Int, Int] = new PartialFunction[Int, Int] {
       //States that this partial function will take on the task
       def isDefinedAt(x: Int) = x % 2 == 0
@@ -23,13 +24,13 @@ class AboutPartialFunctions extends KoanSuite with ShouldMatchers {
 
       def apply(v1: Int) = v1 * 3
     }
-
+    // orElse appears to pertain only to partial functions
     val whatToDo = doubleEvens orElse tripleOdds //Here we chain the partial functions together
 
-    whatToDo(3) should be(__)
-    whatToDo(4) should be(__)
+    whatToDo(3) should be(9)
+    whatToDo(4) should be(8)
   }
-
+  // So radical
   koan( """Case statements are a quick way to create partial functions. When you create a case
           | statement, the apply and isDefinedAt is created for you.""") {
 
@@ -42,8 +43,8 @@ class AboutPartialFunctions extends KoanSuite with ShouldMatchers {
     }
 
     val whatToDo = doubleEvens orElse tripleOdds //Here we chain the partial functions together
-    whatToDo(3) should be(__)
-    whatToDo(4) should be(__)
+    whatToDo(3) should be(9)
+    whatToDo(4) should be(8)
   }
 
   koan( """The result of partial functions can have an \'andThen\' function added to the end
@@ -59,10 +60,11 @@ class AboutPartialFunctions extends KoanSuite with ShouldMatchers {
 
     val addFive = (x: Int) => x + 5
     val whatToDo = doubleEvens orElse tripleOdds andThen addFive //Here we chain the partial functions together
-    whatToDo(3) should be(__)
-    whatToDo(4) should be(__)
+    whatToDo(3) should be(14)
+    whatToDo(4) should be(13)
   }
 
+  // Mother of god...
   koan( """The result of partial functions can have an \'andThen\' function added to the end
           | of the chain used to continue onto another chain of logic""") {
 
@@ -82,7 +84,7 @@ class AboutPartialFunctions extends KoanSuite with ShouldMatchers {
 
     val whatToDo = doubleEvens orElse tripleOdds andThen (printEven orElse printOdd)
 
-    whatToDo(3) should be(__)
-    whatToDo(4) should be(__)
+    whatToDo(3) should be("Odd")
+    whatToDo(4) should be("Even")
   }
 }
